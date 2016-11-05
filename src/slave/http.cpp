9dd588f23819e0a477914cf3d125407b62394f76
Benjamin Hindman <benjamin.hindman@gmail.com>
2016-11-05 00:08:40 -0700

    Changes in Mesos to make http::Request::client optional.

--- src/slave/http.cpp
+++ src/slave/http.cpp
@@ -275,7 +275,7 @@ void Slave::Http::log(const Request& request)
   Option<string> forwardedFor = request.headers.get("X-Forwarded-For");
 
   LOG(INFO) << "HTTP " << request.method << " for " << request.url.path
-            << " from " << request.client
+//             << " from " << request.client
             << (userAgent.isSome()
                 ? " with User-Agent='" + userAgent.get() + "'"
                 : "")
